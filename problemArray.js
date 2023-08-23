db3c7a73f71c595c04d793297619a6eb0eacf4a6
marufbellah <90157254+Maruf-Bellah@users.noreply.github.com>
2023-08-23 18:37:33 +0600

    problem is finsish

--- problemArray.js
+++ problemArray.js
@@ -33,14 +33,57 @@ for (let i = 0; i < myArray.length; i++) {
 
 // array এর মধ্যে শুধু even number এবং তার সাথে যোগফল দেখাও =================================
 
-var arr = [1, 8, 6, 9, 7, 5, 64, 1, 2, 3, 6, 7, 9, 41, 5, 63, 1, 58];
-var sum = 0;
+// var arr = [1, 8, 6, 9, 7, 5, 64, 1, 2, 3, 6, 7, 9, 41, 5, 63, 1, 58];
+// var sum = 0;
 
-for (var i = 0; i < arr.length; i++) {
-  if (arr[i] % 2 === 1) {
-    sum = sum + arr[i]
-    // console.log(arr[i]);
+// for (var i = 0; i < arr.length; i++) {
+//   if (arr[i] % 2 === 1) {
+//     sum = sum + arr[i]
+//     // console.log(arr[i]);
+//   }
+// }
+// console.log(sum);
+
+let myArray6 = [2, 12, 5, 6, 7, 70, 8];
+let sobThakeBoro = 0;
+
+for (let i = 0; i < myArray6.length; i = i + 1) {
+  if (sobThakeBoro < myArray6[i]) {
+    sobThakeBoro = myArray6[i];
   }
+  // console.log(sobThakeBoro);
 }
-console.log(sum);
 
+// console.log(10 > 2);
+
+const names = [
+  "abul",
+  "babul",
+  "abul",
+  "babul",
+  "cabul",
+  "dabul",
+  "ebul",
+  "gabul",
+  "abul",
+  "babul",
+  "gabul",
+  "habul",
+  "jabul",
+  "mokbul"
+];
+
+
+function removeDublicate(names){
+  const unique =[];
+  for(let i = 0; i < names.length; i++){
+    const name = names[i];
+
+    if(unique.includes(name) === false){
+      unique.push(name)
+    }
+  }
+  return unique;
+}
+const show = removeDublicate(names)
+console.log(show);
