b16a39f1e22112a8d99f4a11f666197de823df8a
marufbellah <mdmaruf5511507@gmail.com>
2023-08-24 16:25:00 +0600

    cheke the javascript programmer language

--- assignJavaScript.js
+++ assignJavaScript.js
@@ -6,4 +6,18 @@ function radian_to_degree(radian){
     return radian * (180/pi);
 }
 
-console.log(radian_to_degree(0.7853981633974483));
+// console.log(radian_to_degree(0.7853981633974483));
+
+let programingLanguage= ['PHP', 'Rubi', 'JavaScript','Python', 'C#' , 'C++'];
+
+function checkLanguag(programingLanguage){
+    for(let i = 0; i < programingLanguage.length; i++){
+        let check = programingLanguage[i];
+        if(check === "JavaScript"){
+            console.log(check);
+        }
+    }
+}
+
+checkLanguag(programingLanguage);
+// console.log(show);
