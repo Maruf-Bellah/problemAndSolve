0812119efe2e67ca06e2ed78abe47999a656f972
marufbellah <mdmaruf5511507@gmail.com>
2023-08-25 00:10:16 +0600

    callback function is finish

--- problemLoop.js
+++ problemLoop.js
@@ -88,26 +88,3 @@
 
 // add1(10,20)
 
-// function sawon(friend){
-//   let sum = 5 + 10;
-//   return friend(sum)
-// };
-
-// sawon(maruf)
-
-// function maruf(peram){
-//   console.log(peram);
-// }
-
-
-
-// function addfriend(friend){
-//   let add = 15 + 20;
-//   return friend(add)
-// };
-
-// addfriend(akbarVai);
-
-// function akbarVai(perams){
-//   console.log(perams);
-// }
