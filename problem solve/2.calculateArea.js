a2f067cdf90df1d51e3affe025d7393ade23de67
marufbellah <mdmaruf5511507@gmail.com>
2023-09-10 22:26:32 +0600

    problem solve calculateArea

--- problem solve/2.calculateArea.js
+++ problem solve/2.calculateArea.js
@@ -1,1 +1,22 @@
 // 2. **Calculate Area**: Create a function that calculates and returns the area of a rectangle given its length and width as parameters.
+
+
+
+function calculates(length, width){
+    const area = length * width;
+    return area;
+}
+
+console.log(calculates(5,3));
+
+
+function calculatesRectangleArea(){
+    const area = length * width;
+    return area;
+}
+
+const length = 5;
+const width = 3;
+
+const area = calculatesRectangleArea(width, length);
+console.log('the ractangle is ' + ''  + area);
